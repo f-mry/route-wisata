f24b8d8fa8999e788c9861aa54b56f62180d3f38
FarhanM <farhan.mry@gmail.com>
2020-07-07 22:09:46 +0700

    fix : app use mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,21 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+    "krisna/pkg/mysql"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello from server")
 }
 
+type application struct {
+    errorLog *log.Logger
+    infoLog *log.Logger
+    wisata *mysql.WisataModel
+}
+
 
 func main() {
-    
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
+    errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
+    infoLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
 
-    log.Println("Serving on :4040")
-    err := http.ListenAndServe(":4040", mux)
+    db, err := mysql.OpenDBConnection()
     if err != nil {
-        log.Fatal(err)
+        errorLog.Fatal(err)
     }
+    defer db.Close()
+
+    app := &application{
+        errorLog: errorLog,
+        infoLog: infoLog,
+        wisata: &mysql.WisataModel{DB: db},
+    }
+
+    server := &http.Server{
+        Addr: ":4040",
+        ErrorLog: app.errorLog,
+        Handler: app.routes(),
+    }
+
+    err = server.ListenAndServe()
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+
 }
