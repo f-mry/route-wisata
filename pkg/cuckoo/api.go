26138235b03cce832e7e8aa4a666e1634ac582d8
FarhanM <farhan.mry@gmail.com>
2020-07-09 21:58:36 +0700

    feat : cuckoo api

--- pkg/cuckoo/api.go
+++ pkg/cuckoo/api.go
@@ -1,32 +1,56 @@
-package main
+package cuckoo
 
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	// "io/ioutil"
 	"log"
 	"net/http"
 )
+const 
+    API = "http://127.0.1:5000/api" 
 
-func main() {
 
-    node := []int{1,2,3}
-    nodeMap := map[string]interface{}{"node" : node, "hotel": 4  }
-    jsonVal, err := json.Marshal(nodeMap)
+func GetRoute(node []int, hotelId int) (map[string]interface{}, error) {
+    data := map[string]interface{}{"node" : node, "hotel" : hotelId}
+    jsonData, err := json.Marshal(data)
     if err != nil {
-        log.Fatal(err)
+        log.Println("Build Json Error")
+        return nil, err
     }
 
-    resp, err := http.Post("http://127.0.0.1:5000/api", "application/json", bytes.NewBuffer(jsonVal) )
+    res, err := http.Post(API, "application/json", bytes.NewBuffer(jsonData))
     if err != nil {
-        log.Fatal("HTTP Post: ", err)
+        return nil, err
     }
-    defer resp.Body.Close()
+    defer res.Body.Close()
 
+    var route map[string]interface{}
 
-    var value map[string]interface{}
-    json.NewDecoder(resp.Body).Decode(&value)
+    json.NewDecoder(res.Body).Decode(&route)
 
-    fmt.Printf("%v\n", value)
+    return route, nil
 }
+
+// func main() {
+
+//     node := []int{1,2,3}
+//     nodeMap := map[string]interface{}{"node" : node, "hotel": 4  }
+//     jsonVal, err := json.Marshal(nodeMap)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     resp, err := http.Post("http://127.0.0.1:5000/api", "application/json", bytes.NewBuffer(jsonVal) )
+//     if err != nil {
+//         log.Fatal("HTTP Post: ", err)
+//     }
+//     defer resp.Body.Close()
+
+
+//     var value map[string]interface{}
+//     json.NewDecoder(resp.Body).Decode(&value)
+
+//     fmt.Printf("%v\n", value)
+// }
