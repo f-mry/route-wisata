39a308d30e6450e8142aba924aee92aa0c1e23c1
FarhanM <farhan.mry@gmail.com>
2020-07-09 23:30:09 +0700

    fix : api data models

--- pkg/cuckoo/api.go
+++ pkg/cuckoo/api.go
@@ -3,8 +3,6 @@ package cuckoo
 import (
 	"bytes"
 	"encoding/json"
-	// "fmt"
-	// "io/ioutil"
 	"log"
 	"net/http"
 )
@@ -33,24 +31,3 @@ func GetRoute(node []int, hotelId int) (map[string]interface{}, error) {
     return route, nil
 }
 
-// func main() {
-
-//     node := []int{1,2,3}
-//     nodeMap := map[string]interface{}{"node" : node, "hotel": 4  }
-//     jsonVal, err := json.Marshal(nodeMap)
-//     if err != nil {
-//         log.Fatal(err)
-//     }
-
-//     resp, err := http.Post("http://127.0.0.1:5000/api", "application/json", bytes.NewBuffer(jsonVal) )
-//     if err != nil {
-//         log.Fatal("HTTP Post: ", err)
-//     }
-//     defer resp.Body.Close()
-
-
-//     var value map[string]interface{}
-//     json.NewDecoder(resp.Body).Decode(&value)
-
-//     fmt.Printf("%v\n", value)
-// }
--- pkg/models/model.go
+++ pkg/models/model.go
@@ -11,3 +11,25 @@ type InfoHotel struct {
     Nama string
     
 }
+
+type Route struct {
+	Day1 struct {
+		HotelArrive   []string        `json:"hotelArrive"`
+		HotelInfo     []interface{}   `json:"hotelInfo"`
+		HotelLeaving  []string        `json:"hotelLeaving"`
+		HotelLocation []float64       `json:"hotelLocation"`
+		Location      [][]interface{}     `json:"location"`
+		NodeIndex     [][]interface{} `json:"nodeIndex"`
+		VisitTime     [][]string      `json:"visitTime"`
+	} `json:"day1"`
+	Day2 struct {
+		HotelArrive   []string        `json:"hotelArrive"`
+		HotelInfo     []interface{}   `json:"hotelInfo"`
+		HotelLeaving  []string        `json:"hotelLeaving"`
+		HotelLocation []float64       `json:"hotelLocation"`
+		Location      [][]interface{}     `json:"location"`
+		NodeIndex     [][]interface{} `json:"nodeIndex"`
+		VisitTime     [][]string      `json:"visitTime"`
+	} `json:"day2"`
+	Day3 []interface{} `json:"day3"`
+}
