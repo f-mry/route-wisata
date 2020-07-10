14e17c975ab29c9b15235c5ae88d8112c5a6916e
FarhanM <farhan.mry@gmail.com>
2020-07-10 14:11:40 +0700

    fix : route model

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -13,23 +13,13 @@ type InfoHotel struct {
 }
 
 type Route struct {
-	Day1 struct {
+	Response []struct {
 		HotelArrive   []string        `json:"hotelArrive"`
 		HotelInfo     []interface{}   `json:"hotelInfo"`
 		HotelLeaving  []string        `json:"hotelLeaving"`
 		HotelLocation []float64       `json:"hotelLocation"`
-		Location      [][]interface{}     `json:"location"`
+		Location      [][]float64     `json:"location"`
 		NodeIndex     [][]interface{} `json:"nodeIndex"`
 		VisitTime     [][]string      `json:"visitTime"`
-	} `json:"day1"`
-	Day2 struct {
-		HotelArrive   []string        `json:"hotelArrive"`
-		HotelInfo     []interface{}   `json:"hotelInfo"`
-		HotelLeaving  []string        `json:"hotelLeaving"`
-		HotelLocation []float64       `json:"hotelLocation"`
-		Location      [][]interface{}     `json:"location"`
-		NodeIndex     [][]interface{} `json:"nodeIndex"`
-		VisitTime     [][]string      `json:"visitTime"`
-	} `json:"day2"`
-	Day3 []interface{} `json:"day3"`
+	} `json:"response"`
 }
